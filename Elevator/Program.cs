57164db0360dce986787e6dc4d7c419873d17761
Borislav <borislavatblagoev@gmail.com>
2024-11-21 14:11:17 +0200

    Update Program.cs and Elevator.csproj

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Base baseArea51 = new Base();
+            baseArea51.StartSimulation();
         }
     }
 }
